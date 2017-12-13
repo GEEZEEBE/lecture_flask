4500aefc56756607c03f48b8a9c248d38d1caf43
SanghunOh <otter.oh@gmail.com>
2017-12-13 17:41:20 +0900

    apply blueprint

--- Lecture_Flask/blueprint_main.py
+++ Lecture_Flask/blueprint_main.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 import blueprint_sub
 
 app = Flask(__name__)
-app.register_blueprint(blueprint_sub.app)    #, url_prefix='/pages'
 
-@app.route('/template_base/<user>')
+# register blueprints
+app.register_blueprint(blueprint_sub.app, url_prefix='/bp_subs')
+# app.register_blueprint(blueprint_sub.app, url_prefix=blueprint_sub.app.name)
+# app.register_blueprint(blueprint_sub.app)    
+
+@app.route('/blueprint_base/<user>')
 def template_base(user):
    return render_template('template_base.html', myname = user)
 
--- Lecture_Flask/blueprint_sub.py
+++ Lecture_Flask/blueprint_sub.py
@@ -1,17 +1,17 @@
-from flask import Blueprint, Flask, redirect, url_for, request
-app = Blueprint('blueprint_sub', __name__)
+from flask import Blueprint, Flask, redirect, url_for, request, render_template
 
-@app.route('/httpmethod_success/<myname>')
+# app = Flask(__name__)
+app = Blueprint('/subs', __name__)
+
+@app.route('/blueprint_success/<myname>')
 def httpmethod_success(myname):
     return 'welcome '+ myname
 
-@app.route('/httpmethod',methods = ['POST', 'GET'])
-def httpmethod():
-    if request.method == 'POST':
-        user = request.form['myname']
-    else:
-        user = request.args.get('myname')
-    return redirect(url_for('httpmethod_success',myname = user))
+@app.route('/blueprint_base/<user>',methods = ['POST', 'GET'])
+def httpmethod(user):
+#     return redirect(url_for('blueprint_sub.httpmethod_success',myname = user))
+#     return 'welcome '+ user
+    return render_template('template_base.html', myname = user)
 
 if __name__ == '__main__':
     app.run(debug = True)
